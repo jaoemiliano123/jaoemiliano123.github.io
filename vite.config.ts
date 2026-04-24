a0fde7e1297ef90a3ee654baffd3d43fce6248df
JaoEmiliano <joaomanomouse@gmail.com>
2026-04-23 21:40:59 -0300

    Workflow npm install fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig as defineVitestConfig } from "vitest/config";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test:{
-    globals: true,
-    environment: 'jsdom',
-    setUpFiles: './src/setupTests.ts',
-  }
-})
+export default defineVitestConfig({
+ plugins: [react()],
+ test: {
+ globals: true,
+ environment: "jsdom",
+ setupFiles: "./src/test/setup.ts",
+ },
+});
